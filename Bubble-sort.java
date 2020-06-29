914dda4faeaf23c7e3136504d8cd20ec3bc5ca03
vitorsinedino <vitorsinedino@gmail.com>
2020-06-29 09:02:41 -0300

    bubble sort adition

---
+++ Bubble-sort.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+class QuickStart {
+    public static void main(String[] args) {
+
+        int[] intArray = { 20, -11, 35, -6, 17, 55 };
+
+        for (int lastUnsorted = intArray.length - 1; lastUnsorted > 0; lastUnsorted--) {
+            for (int i = 0; i < lastUnsorted; i++) {
+                if (intArray[i] > intArray[i + 1]) {
+                    swap(intArray, i, i + 1);
+                }
+            }
+        }
+        for (int i = 0; i < intArray.length - 1; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        if (i == j) {
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
--- quickstart.java
+++ quickstart.java
@@ -2,21 +2,6 @@ import java.util.Scanner;
 
 class QuickStart {
     public static void main(String[] args) {
-        int age = 0;
-        int guess = 0;
-        Scanner input = new Scanner(System.in);
-        System.out.println("im an old god, try to guess my age");
-        age = (int) (Math.round(Math.random() * 100));
-        while (guess != age) {
-            guess = input.nextInt();
-            if (guess > age) {
-                System.out.println("I'm not that old !!!");
-            } else if (guess < age) {
-                System.out.println("I'm not young, child !!");
-            }
-        }
-        ;
-        System.out.println("Congratulations mortal !!");
 
     }
 }
