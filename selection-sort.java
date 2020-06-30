cfa3a58a30eebf7eb4dded40308897fb6ebfb9ae
vitorsinedino <vitorsinedino@gmail.com>
2020-06-30 10:10:02 -0300

    selection sort adition

--- selection-sort.java
+++ selection-sort.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 
-class bubbleSort {
+class selectionSort {
     public static void main(String[] args) {
 
         int[] intArray = { 20, -11, 35, -6, 17, 55 };
 
         for (int lastUnsorted = intArray.length - 1; lastUnsorted > 0; lastUnsorted--) {
-            for (int i = 0; i < lastUnsorted; i++) {
-                if (intArray[i] > intArray[i + 1]) {
-                    swap(intArray, i, i + 1);
+            int larger = 0;
+            for (int i = 1; i <= lastUnsorted; i++) {
+                if (intArray[larger] < intArray[i]) {
+                    larger = i;
                 }
             }
+            swap(intArray, larger, lastUnsorted);
         }
         for (int i = 0; i < intArray.length; i++) {
             System.out.println(intArray[i]);
